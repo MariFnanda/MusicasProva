e4fa86589666b8d41d2f0f037d5e0d223aaa6894
MariFnanda <117872164+MariFnanda@users.noreply.github.com>
2023-02-12 10:17:57 -0300

    Delete musica-api directory

--- musica-api/src/main/java/com/tech4me/crudapi/repository/MusicaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.tech4me.crudapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.tech4me.crudapi.model.Musica;
-
-public interface MusicaRepositorio extends MongoRepository<Musica,String>{
-    
-}
